c74162270030c95e92d68a086b2cb773b4e518d9
brendan-e-mckinley <brendan_mckinley@mines.edu>
2025-09-17 22:57:28 -0600

    use more intuitive ordering (to me, anyway)

--- staggered_grid_practice.m
+++ staggered_grid_practice.m
@@ -21,35 +21,35 @@ N_V = Nx * Ny_minus;
 N_P = Nx * Ny;
 
 %% BCs
-V_bottom = -3.5;
-V_top    = -3.5;
-f_bc_mat = zeros(Nx, Ny);         
-g_bc_mat = zeros(Nx, Ny_minus);   
-h_bc_mat = zeros(Nx, Ny);    
+V_lower = -3.5;
+V_upper    = -3.5;
+f_bc_mat = zeros(Ny, Nx);          
+g_bc_mat = zeros(Ny_minus, Nx);    
+h_bc_mat = zeros(Ny, Nx);          
 
 f_bc = f_bc_mat(:);
-g_bc_mat(:,1) = V_bottom / dy^2;
-g_bc_mat(:,end) = V_top / dy^2;
+g_bc_mat(1,:)   = V_lower / dy^2;
+g_bc_mat(end,:) = V_upper / dy^2;
 g_bc = g_bc_mat(:);
-h_bc_mat(:,1) = - V_bottom / dy;
-h_bc_mat(:,end) =  V_top / dy;
+h_bc_mat(1,:)   = - V_lower / dy;
+h_bc_mat(end,:) =  V_upper / dy;
 h_bc = h_bc_mat(:);
 
 %% Sigma
 compute_sigma = @(xx, yy) 50 * (sin(2*pi*xx).*sin(4*pi*yy) + 1);
 
-sigma_matrix_U = zeros(Nx, Ny);
-for i = 1:Nx
-    for j = 1:Ny
-        sigma_matrix_U(i,j) = compute_sigma(xint(i), y_j_U(j));
+sigma_matrix_U = zeros(Ny, Nx); 
+for j = 1:Ny
+    for i = 1:Nx
+        sigma_matrix_U(j,i) = compute_sigma(xint(i), y_j_U(j));
     end
 end
 sigma_U_diag = spdiags(sigma_matrix_U(:), 0, N_U, N_U);
 
-sigma_matrix_V = zeros(Nx, Ny_minus);
-for i = 1:Nx
-    for j = 1:Ny_minus
-        sigma_matrix_V(i,j) = compute_sigma(x_i_V(i), yint(j) + dy);
+sigma_matrix_V = zeros(Ny_minus, Nx);
+for j = 1:Ny_minus
+    for i = 1:Nx
+        sigma_matrix_V(j,i) = compute_sigma(x_i_V(i), yint(j) + dy);
     end
 end
 sigma_V_diag = spdiags(sigma_matrix_V(:), 0, N_V, N_V);
@@ -71,18 +71,18 @@ D2_y_U = D2_y_U / dy^2;
 D2_y_V = spdiags([ones(Ny_minus,1), -2*ones(Ny_minus,1), ones(Ny_minus,1)], [-1 0 1], Ny_minus, Ny_minus) / dy^2;
 
 %% Laplacians
-Lap_U = kron(speye(Ny), D2_x) + kron(D2_y_U, speye(Nx));    % N_U x N_U
-Lap_V = kron(speye(Ny_minus), D2_x) + kron(D2_y_V, speye(Nx)); % N_V x N_V
+Lap_U = kron(D2_x, speye(Ny)) + kron(speye(Nx), D2_y_U);
+Lap_V = kron(D2_x, speye(Ny_minus)) + kron(speye(Nx), D2_y_V);
 
 %% Gradients
 D_x_forward = spdiags([-ones(Nx,1), ones(Nx,1)], [0 1], Nx, Nx);
 D_x_forward(end,1) = 1;
 D_x_forward = D_x_forward / dx;
-G_x = kron(speye(Ny), D_x_forward);
+G_x = kron(D_x_forward, speye(Ny));
 
 D_y_small = spdiags([-ones(Ny_minus,1), ones(Ny_minus,1)], [0 1], Ny_minus, Ny);
 D_y_small = D_y_small / dy;
-G_y = kron(D_y_small, speye(Nx));
+G_y = kron(speye(Nx), D_y_small);
 
 % Divergence
 D_x = -G_x.';  % N_P x N_U
@@ -91,7 +91,7 @@ D_y = -G_y.';  % N_P x N_V
 %% Saddle point system
 A = [ Lap_U + sigma_U_diag     zeros(N_U, N_V)          G_x; 
       zeros(N_V, N_U)          Lap_V + sigma_V_diag     G_y;
-      D_x                      D_y                      zeros(N_U, N_U) ];
+      D_x                      D_y                      zeros(N_P, N_P) ];
 
 RHS = [ f_bc;
         g_bc; 
@@ -106,25 +106,24 @@ V = sol(N_U+1:N_U+N_V);
 P = sol(N_U+N_V+1:end);
 
 %% Plot
-Uplot = reshape(U, [Nx, Ny]);
-Vplot = reshape(V, [Nx, Ny_minus]);
-Pplot = reshape(P, [Nx, Ny]);         
+Uplot = reshape(U, [Ny, Nx]);
+Vplot = reshape(V, [Ny_minus, Nx]);
+Pplot = reshape(P, [Ny, Nx]);
 
-UFull = NaN(Nx+2, Ny+2);
-UFull(2:Nx+1, 2:Ny+1) = Uplot;
-UFull(:,1) = UFull(:,Nx);
-UFull(:,Nx+1) = UFull(:,2);
+UFull = NaN(Ny+2, Nx+2);
+UFull(2:Ny+1, 2:Nx+1) = Uplot;
+UFull(1,:) = UFull(2,:); 
+UFull(end,:) = UFull(end-1,:);
 
-VFull = V_bottom * ones(Nx+2, Ny+2);
-VFull(2:Nx+1, 2:Ny) = -Vplot; 
-VFull(:,1) = VFull(:,Nx); 
-VFull(:,Nx+1) = VFull(:,2);
+VFull = V_lower * ones(Ny+2, Nx+2);
+VFull(2:Ny, 2:Nx+1) = -Vplot; 
+VFull(1,:) = VFull(2,:); 
+VFull(end,:) = VFull(end-1,:);
 
 xplot = linspace(0,L,Nx+2);
 yplot = linspace(0,L,Ny+2);
-[Xplot, Yplot] = meshgrid(yplot, xplot);
+[Xplot, Yplot] = meshgrid(xplot, yplot);
 
-figure; surf(Xplot, Yplot, UFull, 'EdgeColor','none'); title('U'); xlabel('y'); ylabel('x');
-figure; surf(Xplot, Yplot, VFull, 'EdgeColor','none'); title('V'); xlabel('y'); ylabel('x');
+figure; surf(Xplot, Yplot, UFull, 'EdgeColor','none'); title('U'); xlabel('x'); ylabel('y');
+figure; surf(Xplot, Yplot, VFull, 'EdgeColor','none'); title('V'); xlabel('x'); ylabel('y');
 figure; surf(Xplot, Yplot, sqrt(UFull.^2 + VFull.^2), 'EdgeColor','none'); title('|velocity|');
-
